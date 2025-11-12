601b533d7576c87c6209a8a91a07094eddecc0ab
Elias <elias.makela@edu.tampere.fi>
2025-11-12 10:55:21 +0200

    12.11

---
+++ bruno-elias-projekti25/Assets/Script/Globals.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Globals : MonoBehaviour
+{
+
+    public static int player1Score = 0;
+    public static int player2Score = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
